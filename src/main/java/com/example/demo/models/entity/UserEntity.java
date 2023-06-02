a456d2ba0b9f265264a4135966c62898489e61dc
Mochalka <bdauren0408@gmail.com>
2023-06-02 11:50:10 +0600

    add UserEntity structure

--- src/main/java/com/example/demo/models/entity/UserEntity.java
+++ src/main/java/com/example/demo/models/entity/UserEntity.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 
+import java.math.BigInteger;
+import java.sql.Timestamp;
 import java.time.LocalDate;
 
 @Entity
@@ -14,10 +17,16 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(length = 255)
     private String login;
+    @Column(length = 255)
+    private String password;
+    @Column(length = 255)
+    private String email;
     @CreationTimestamp
-    private LocalDate createdDatetime;
+    private Timestamp createdDatetime;
     @UpdateTimestamp
-    private LocalDate updatedDatetime;
-
+    private Timestamp updatedDatetime;
+    @Column(length = 255)
+    private String role;
 }
